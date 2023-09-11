937d8af7c61026ca50ec8ef85daa7bca86dea5c5
Artem Dorozhkin <artemcool6554@gmail.com>
2023-09-11 21:54:25 +0200

    Added scoring mechanism with TextMeshPro UI
    - Integrated TextMeshPro to display the score.
    - Added `BallTracker` script to the platform to detect balls and update the score.
    - Adjusted the `BallSpawner` script to spawn balls with specific scoring colors.
    - Ensured balls have rigidbodies and colliders for interaction with platform trigger.
    - Linked TextMeshPro UI component to `BallTracker` for dynamic score updates."

--- BALLGAME/Assets/BallSpawner.cs
+++ BALLGAME/Assets/BallSpawner.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class BallSpawner : MonoBehaviour
 {
     public GameObject ball;  // The ball we want to spawn
-    public Transform bottomBorder; // The bottom border where the balls should be checked
     public float spawnTime = 2.0f;  // How often we want to spawn the ball
     public Vector3 spawnPosition; // The high position where balls should spawn from
 
@@ -15,35 +14,10 @@ public class BallSpawner : MonoBehaviour
         new Color(0, 1, 0)  // Green
     };
 
-    private int score = 0; // Store the score
-
     void Start()
     {
         // Start calling the Spawn function repeatedly after a delay of spawnTime and then every spawnTime seconds
         InvokeRepeating("Spawn", spawnTime, spawnTime);
-    }
-
-    void Update()
-    {
-        // Check if any ball has reached the bottom border and update the score
-        foreach (GameObject ballObj in GameObject.FindGameObjectsWithTag("Ball"))
-        {
-            if (ballObj.transform.position.y < bottomBorder.position.y)
-            {
-                Color ballColor = ballObj.GetComponent<Renderer>().material.color;
-                foreach (Color color in scoringColors)
-                {
-                    if (color == ballColor)
-                    {
-                        score++;
-                        Debug.Log("Score: " + score);
-                        break;
-                    }
-                }
-
-                Destroy(ballObj);
-            }
-        }
     }
 
     void Spawn()
@@ -59,6 +33,7 @@ public class BallSpawner : MonoBehaviour
         rend.material.color = scoringColors[Random.Range(0, scoringColors.Length)];
     }
 }
+
 
 
 
